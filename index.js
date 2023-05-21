8ba31ea59ff9963c62c31a267950552578704cb0
Yongwoo Lee <yw6636@gmail.com>
2023-05-21 17:46:57 -0400

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const server = express();
+const express = require('express')
 
-server.use(express.json());
+const server = express()
 
+server.use(express.json())
 server.get('/hello', (req, res) => {
-  res.json('hello, there');
-});
+  res.json('hello there')
+})
 
-server.use('*', (req, res) => {
-  res.json({ message: 'API is UP!' })
-});
+const port = process.env.PORT || 9000
 
-server.listen(9000, () => {
-  console.log("\n*** Server Running on http://localhost:9000 ***\n");
-});
+server.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
